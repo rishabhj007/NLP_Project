7a51bc344f1d1e908d270e253aef950a63815948
Rishabh Jain <rishabhj007@hotmail.com>
2020-06-03 10:59:52 +0530

    added option for number of tweets.

--- scrapping.py
+++ scrapping.py
@@ -1,15 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 
-hashTag = input()             # HashTag input from user
-hashTag.strip()               # removing any whitespaces
-if hashTag[0] == '#':         # removing hashTag char if present
-    hashTag = hashTag[1:]
-hashTag.replace(" ", "")      # removing spaces
 
-url = "https://mobile.twitter.com/search?q=" + "%23" + hashTag         # twitter query url
+def gettweets(html):
+    thislist = []
+    tweets = html.findAll("div", {"class": "dir-ltr"})
+    for tweet in tweets:
+        thislist.append(tweet)
+    return thislist
+
+
+print("Please input the search hashtag")
+hashTag = str(input())  # HashTag input from user
+print("Please input the required tweets")
+tweetNo = int(input())  # No of tweets required input from user
+tweetlist = []           # Empty list to contain tweets as strings
+hashTag.strip()  # removing any whitespaces
+if hashTag[0] == '#':  # removing hashTag char if present
+    hashTag = hashTag[1:]
+hashTag.replace(" ", "")  # removing spaces
+
+url = "https://mobile.twitter.com/search?q=" + "%23" + hashTag  # twitter query url
 print(url)
-data = requests.get(url)                                        # getting the html of the search results
-html = BeautifulSoup(data.text, 'html.parser')                  # changing raw data to html
-print(html.get_text())
+data = requests.get(url)  # getting the html of the search results
+html = BeautifulSoup(data.text, 'html.parser')  # changing raw data to html
+# print(html.get_text())
+while True:
+    if tweetlist.count() < tweetNo:
+        tweetlist.append(gettweets(html))
+        Div = html.find("div", {"class": "w-button-more"}).find('a')
+        url = Div['href']
+        data = requests.get(url)
+        html = BeautifulSoup(data.text,'html_parser')
+    else:
+        break
+
 
