148def16a32972b8067712ba8580a08ebdb4b625
Rishabh Jain <rishabhj007@hotmail.com>
2020-06-23 12:32:47 +0530

    minor fixes

--- classifier2.py
+++ classifier2.py
@@ -2,12 +2,19 @@ import pandas as pd
 from nltk import WordNetLemmatizer
 from nltk.corpus import stopwords
 import re
+import string
+import random
 
 cols = ['sentiment','id','date','query_string','user','text']
-loc = "C:/Users/risha/Downloads/"
+loc = "C:/Users/risha/Downloads/NLP Data/"
 df = pd.read_csv(loc+"emoticon.csv",header = None, names=cols,encoding = "ISO-8859-1")
 df = df.drop(columns=['id','date','query_string','user'])
+neg = random.randint(1,650000)
+pos = random.randint(800000,1500000)
 
+dataset = df.loc[neg:neg+100000,:] + df.loc[pos:pos+100000,:]
+print(len(dataset))
+print(dataset.head())
 # k = 0
 # for i in df.text:
 #     print(type(i))
@@ -17,34 +24,42 @@ df = df.drop(columns=['id','date','query_string','user'])
 #         break
 
 # df = df.iloc[:10,]
-# print(df)
+print("Data Import Complete")
 
 lem = WordNetLemmatizer()
 sw_set = set(stopwords.words('english'))
 dataset = []
 for i in df.text:
     tweet = i.lower()
-    token2 = re.sub('[^a-zA-Z]', ' ', tweet).split()
-    token2 = [lem.lemmatize(word) for word in token2 if word not in sw_set]
-    dataset.append(' '.join(token2))
-#
-# print(dataset)
+    tweet = re.sub(r"\A@\w+",'',tweet)
+    tweet = re.sub(r"https\S+", '', tweet)
+    tweet = re.sub(r"http\S+", '', tweet)
+    tweet = tweet.strip(string.punctuation)
+    token2 = re.sub('[^a-zA-Z]',' ', tweet).split()
+    # print(token2)
+    token3 = [lem.lemmatize(word) for word in token2 if word not in sw_set]
+    # print(' '.join(token3))
+    dataset.append(' '.join(token3))
+    # if(len(dataset) >25):
+    #     break
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-cv = TfidfVectorizer(max_features=1500,min_df= 52)
+print("Cleaning Done")
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
 X = cv.fit_transform(dataset).toarray()
 y = df.iloc[:, 0].values
 #
 # print(cv.get_feature_names())
-
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
-
-# Training the Naive Bayes model on the Training set
-import sklearn.naive_bayes as nb
-classifier = nb.GaussianNB()
-classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
+#
+# # Splitting the dataset into the Training set and Test set
+# from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+#
+# # Training the Naive Bayes model on the Training set
+# import sklearn.naive_bayes as nb
+# classifier = nb.GaussianNB()
+# classifier.fit(X_train, y_train)
+#
+# # Predicting the Test set results
+# y_pred = classifier.predict(X_test)
